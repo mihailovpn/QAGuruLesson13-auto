42a914537ae735aceff90429896dea583051fef0
Павел Михайлов <pavel.mikhaylov@simbirsoft.com>
2021-11-22 22:13:21 +0300

    .gitignore fix

--- src/test/java/cloud/autotests/config/App.java
+++
@@ -1,7 +0,0 @@
-package cloud.autotests.config;
-
-import org.aeonbits.owner.ConfigFactory;
-
-public class App {
-    public static AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
-}
--- src/test/java/cloud/autotests/tests/pages/MainPage.java
+++
@@ -1,6 +0,0 @@
-package cloud.autotests.tests.pages;
-
-public class MainPage {
-    public String mainPageLogoLocator = ".display-1";
-    public String mainPageLogoText = "Selenium automates browsers. That's it!";
-}
