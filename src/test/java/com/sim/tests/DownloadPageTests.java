2ce6f8455ff630e5e5fe1609cb9f3ea78a79c5a4
Павел Михайлов <pavel.mikhaylov@simbirsoft.com>
2021-11-22 22:19:52 +0300

    deleted some files

---
+++ src/test/java/com/sim/tests/DownloadPageTests.java
@@ -0,0 +1,77 @@
+package com.sim.tests;
+
+import com.sim.helpers.DriverUtils;
+import com.sim.tests.pages.DownloadPage;
+import com.sim.tests.pages.MainPage;
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class DownloadPageTests extends TestBase {
+
+    DownloadPage downloadPage = new DownloadPage();
+    MainPage mainPage = new MainPage();
+
+
+    @Test
+    @Feature("DownloadPage")
+    @AllureId("5892")
+    @DisplayName("Page have Main page button and it works")
+    void MainPageButtonTest() {
+        step("Open url " + downloadPage.url, () ->
+                open(downloadPage.url));
+
+        step("Click Main Page button", () -> {
+            downloadPage.clickMainPageButton();
+            $(mainPage.mainPageLogoLocator).should(Condition.text(mainPage.mainPageLogoText));
+        });
+    }
+
+    @Test
+    @Feature("DownloadPage")
+    @AllureId("5893")
+    @DisplayName("Page have Browser Support button")
+    void browserSupportButtonTest() {
+        step("Open url " + downloadPage.url, () ->
+                open(downloadPage.url));
+
+        step("Click Browser Support button", () -> {
+            downloadPage.clickBrowserSupportButton();
+        });
+    }
+
+    @Test
+    @DisplayName("Page title should have header text")
+    void titleTest() {
+        step("Open url " + downloadPage.url, () ->
+            open(downloadPage.url));
+
+        step("Page title should have text 'Downloads | Selenium'", () -> {
+            String expectedTitle = "Downloads | Selenium";
+            String actualTitle = title();
+
+            assertThat(actualTitle).isEqualTo(expectedTitle);
+        });
+    }
+
+    @Test
+    @DisplayName("Page console log should not have errors")
+    void consoleShouldNotHaveErrorsTest() {
+        step("Open url " + downloadPage.url, () ->
+            open(downloadPage.url));
+
+        step("Console logs should not contain text 'SEVERE'", () -> {
+            String consoleLogs = DriverUtils.getConsoleLogs();
+            String errorText = "SEVERE";
+
+            assertThat(consoleLogs).doesNotContain(errorText);
+        });
+    }
+}
---
+++ src/test/java/com/sim/tests/pages/MainPage.java
@@ -0,0 +1,6 @@
+package com.sim.tests.pages;
+
+public class MainPage {
+    public String mainPageLogoLocator = ".display-1";
+    public String mainPageLogoText = "Selenium automates browsers. That's it!";
+}
